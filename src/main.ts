8f4bc6644cd691ebda395f44211477500d85cd79
Ruslan Galiev <ghaliev@gmail.com>
2023-10-29 18:47:41 +0500

    initial commit for repo

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import { App } from './app';
+import { LoggerService } from './logger/logger.service';
+import { UserController } from './users/users.controller';
+import { ExceptionFilter } from './error/exception.filter';
+import { Container, ContainerModule, interfaces } from 'inversify';
+import { ILogger } from './logger/logger.interface';
+import { TYPES } from './types';
+import { IExceptionFilter } from './error/exception.filter.interface';
+import { UserService } from './users/users.service';
+import { IUsersService } from './users/users.service.interface';
+import { IUserController } from './users/users.controller.interface';
+import { IConfigService } from './config/config.service.interface';
+import { ConfigService } from './config/config.service';
+import { PrismaService } from './database/prisma.service';
+import { UsersRepository } from './users/users.repository';
+import { IUsersRepository } from './users/users.repository.interface';
+
+export interface IBootstrapReturn {
+	appContainer: Container;
+	app: App;
+}
+
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+	bind<ILogger>(TYPES.ILogger).to(LoggerService).inSingletonScope();
+	bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
+	bind<IUserController>(TYPES.UserController).to(UserController);
+	bind<IUsersService>(TYPES.UserService).to(UserService);
+	bind<PrismaService>(TYPES.PrismaService).to(PrismaService).inSingletonScope();
+	bind<IConfigService>(TYPES.ConfigService).to(ConfigService).inSingletonScope();
+	bind<IUsersRepository>(TYPES.UsersRepository).to(UsersRepository).inSingletonScope();
+	bind<App>(TYPES.Application).to(App);
+});
+
+async function bootstrap(): Promise<IBootstrapReturn> {
+	const appContainer = new Container();
+	appContainer.load(appBindings);
+	const app = appContainer.get<App>(TYPES.Application);
+	await app.init();
+	return { appContainer, app };
+}
+
+export const boot = bootstrap();
